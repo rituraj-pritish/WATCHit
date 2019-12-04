0dd4b8e41f5ca4a397db496fb767a7727285ae37
rituraj-pritish <riturajpritish@gmail.com>
2019-12-04 22:04:21 +0530

    changed config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,5 @@
-import dev from './dev'
-import prod from './prod'
-
-const config = process.env.NODE_ENV === 'production' ? prod : dev
-
-export default config
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod')
+}else {
+  module.exports = require('./dev')
+}
